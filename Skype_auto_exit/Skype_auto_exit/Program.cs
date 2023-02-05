8a40b2f8be1c92857aa597268a0538062874cd0d
APP서비스팀_박현호 <phh_92@naver.com>
2023-02-05 23:21:47 +0900

    first commit

---
+++ Skype_auto_exit/Skype_auto_exit/Program.cs
@@ -0,0 +1,10 @@
+using Skype_auto_exit;
+
+public class main
+{
+    public static void Main(string[] args)
+    {
+        SkypeProcessKill run = new SkypeProcessKill();
+        run.RunApp();
+    }
+}
---
+++ Skype_auto_exit/Skype_auto_exit/SkypeProcessKill.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skype_auto_exit
+{
+    internal class SkypeProcessKill
+    {
+
+        public void RunApp()
+        {
+            Console.WriteLine("Hello, Skype Auto Exit Program!");
+            while (true)
+            {
+                SkypeProcessKill.KillSkypeProcess();
+                Thread.Sleep(5000);
+            }
+        }
+
+
+        /*
+        * skype process 를 kill 하는 명령어를 수행하는 Method
+        */
+        private static void KillSkypeProcess()
+        {
+            string cmd = @"taskkill /f /im lync.exe";
+
+            ProcessStartInfo pri = new ProcessStartInfo();
+            Process pro = new Process();
+            pri.FileName = "cmd.exe";
+            pri.WorkingDirectory= @"C:\";
+            pri.CreateNoWindow = false;
+            pri.UseShellExecute = false;
+            pri.RedirectStandardInput = true;
+            pri.RedirectStandardOutput = true;
+            pri.RedirectStandardError = true;
+           
+            pro.StartInfo = pri;
+            pro.Start();
+
+            pro.StandardInput.Write(cmd+Environment.NewLine);
+            pro.StandardInput.Close();
+            pro.Close();
+        }
+
+    }
+}
